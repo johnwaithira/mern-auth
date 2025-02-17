2a4544cd35e5186e24bd41f954f452fcff18b0da
johnwaithira <waithirajon@gmail.com>
2025-02-17 23:34:00 +0300

    FIREBASE ENV

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+const VITE_FIREBASE_API_KEY="AIzaSyCez9bU-qIFqVxc6c0YjTIrlmGAjDy8zYE"
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: VITE_FIREBASE_API_KEY,
   authDomain: "mern-auth-4a970.firebaseapp.com",
   projectId: "mern-auth-4a970",
   storageBucket: "mern-auth-4a970.firebasestorage.app",
   messagingSenderId: "195827320854",
   appId: "1:195827320854:web:e2a16d24c6cc3bdbf3cd37"
 };
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
